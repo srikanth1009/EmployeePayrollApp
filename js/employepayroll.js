b3eaa17766c3331eca4bbec1fc5afc80be8bad67
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-19 12:09:39 +0530

    Added: Event listner for Salary Range to Show Appropriate value

---
+++ js/employepayroll.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    salaryOutput();
+
+});
+
+function salaryOutput() {
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+
+    });
+}
