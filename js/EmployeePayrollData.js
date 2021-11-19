5b596bc2f9d1d5a633df39419b487aa43c95aa42
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-19 12:23:54 +0530

    Added: On submit form Populating Employee Object

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,86 @@
+class EmployeePayrollData {
+    id;
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Name is Incorrect";
+    }
+    get profilePic() {
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+
+        this._salary = salary;
+
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+    }
+}
+
+function save() {
+
+    let employeeName = document.querySelector('#name').value;
+    let profileList = document.querySelectorAll('input[name="profile"]');
+    let employeeProfileImage;
+    for (let image of profileList) {
+        if (image.checked) {
+            employeeProfileImage = image.value;
+            break;
+        }
+    }
+
+
+}
