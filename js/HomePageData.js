42a015e60ed49cd1ed2a0f99319703ffd0b71de4
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-19 14:03:09 +0530

    Added: View Employee Payroll details in a tabular format from json ob…

--- js/HomePageData.js
+++ js/HomePageData.js
@@ -4,27 +4,75 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 let createInnerHtml = () => {
     let headerHTML = "<tr> <th></th> <th>Name</th> <th>Gender</th> <th>Department</th> <th>Salary</th> <th>StartDate</th><th>Actions</th> </tr>"
-    let innerHTML = `
-    ${headerHTML}
+    let innerHTML = `${headerHTML}`;
+    let empPayrollList = createEmployeePayrollJSON();
+
+    for (const empPayrollData of empPayrollList) {
+        innerHTML = `
+        ${innerHTML}
+    <tr>
+    <td>
+        <img class="profile" src="${empPayrollData._profilePic}">
     
-<tr>
-<td>
-    <img class="profile" src="../assets/profile-images/Ellipse -3.png">
-</td>
-<td> 
-    Prashanth N
-</td>
-<td>Male</td>
-<td>
-    <div class="dept-label">Engineer</div>
-    <div class="dept-label">Others</div>
-</td>
-<td>400000</td>
-<td>16 Oct 2021</td>
-<td>
-    <img alt="delete" src="../assets/icons/delete-black-18dp.svg">
-    <img alt="edit" src="../assets/icons/create-black-18dp.svg">
-</td>
-  </tr>  `
+    </td>
+    <td> 
+       ${empPayrollData._name}
+    </td>
+    <td>${empPayrollData._gender}</td>
+    <td>
+        ${getDeptHtml(empPayrollData._department)}
+    </td>
+    <td>${empPayrollData._salary}</td>
+    <td>${empPayrollData._startDate}</td>
+    <td>
+        <img alt="delete" src="../assets/icons/delete-black-18dp.svg">
+        <img alt="edit" src="../assets/icons/create-black-18dp.svg">
+    </td>
+      </tr>  `;
+    }
+
     document.querySelector("#display").innerHTML = innerHTML;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class = 'dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollDB = [
+
+        {
+            "_id": 1,
+            "_name": "Prashanth N",
+            "_gender": "male",
+            "_department": [
+                "Engineer",
+                "Other"
+            ],
+            "_salary": "498700",
+            "_startDate": "16 Oct 2021",
+            "_note": "All In One",
+            "_profilePic": "../assets/profile-images/Ellipse -3.png"
+        },
+        {
+            "_id": 5,
+            "_name": "Jyothi",
+            "_gender": "female",
+            "_department": [
+                "Sales",
+                "Finance"
+            ],
+            "_salary": "400000",
+            "_startDate": "12 Oct 2019",
+            "_note": "",
+            "_profilePic": "../assets/profile-images/Ellipse -1.png",
+            "id": 3
+        }
+
+    ];
+    return empPayrollDB;
 }
